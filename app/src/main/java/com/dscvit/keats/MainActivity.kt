0fbab4f0b577c56e97054686184696425842237e
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2021-02-24 06:07:30 +0530

    feat: add hilt application and provide Android Entry Point

--- app/src/main/java/com/dscvit/keats/MainActivity.kt
+++ app/src/main/java/com/dscvit/keats/MainActivity.kt
@@ -3,7 +3,9 @@ package com.dscvit.keats
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dscvit.keats.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
