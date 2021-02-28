1609f609f5cd06eab4313a20ff2a7241b09457fa
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2021-03-01 04:08:26 +0530

    feat: added firebase otp but pending mvvm

--- app/src/main/java/com/dscvit/keats/ui/auth/SignInFragment.kt
+++ app/src/main/java/com/dscvit/keats/ui/auth/SignInFragment.kt
@@ -1,12 +1,16 @@
 package com.dscvit.keats.ui.auth
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dscvit.keats.databinding.FragmentSignInBinding
-import com.dscvit.keats.utils.shortToast
+import com.dscvit.keats.firebase.AuthHelper
+import com.dscvit.keats.utils.Constants
+import com.dscvit.keats.utils.PreferenceHelper
+import com.dscvit.keats.utils.PreferenceHelper.set
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,16 +22,18 @@ class SignInFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentSignInBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPreferences: SharedPreferences =
+            PreferenceHelper.customPrefs(requireContext(), Constants.PREF_NAME)
         binding.sendOtp.setOnClickListener {
-            val phone = binding.phoneNumber.text.toString()
-            context?.shortToast(phone)
+            val authHelper = AuthHelper(requireContext(), view, requireActivity())
+            authHelper.sendOtp(binding.phoneNumber)
+            sharedPreferences[Constants.PREF_PHONE_NUMBER] = binding.phoneNumber.text.toString()
         }
     }
 }
