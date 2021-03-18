2140344a49c7cbbcfded1e8ac69adf10aee7fec6
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2021-03-18 07:29:51 +0530

    feat: styling styled sign in fragment styled verify otp fragment created splash screen

---
+++ app/src/main/java/com/dscvit/keats/ui/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.dscvit.keats.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.dscvit.keats.databinding.ActivitySplashScreenBinding
+import com.dscvit.keats.utils.Constants
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            Constants.SPLASH_SCREEN_DELAY
+        )
+    }
+}
