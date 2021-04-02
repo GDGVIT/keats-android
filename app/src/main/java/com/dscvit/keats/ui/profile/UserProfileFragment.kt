356fab328667aa40d81f884f4beb474ca605a48a
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2021-04-02 22:17:24 +0530

    feat: add logout

--- app/src/main/java/com/dscvit/keats/ui/profile/UserProfileFragment.kt
+++ app/src/main/java/com/dscvit/keats/ui/profile/UserProfileFragment.kt
@@ -1,9 +1,12 @@
 package com.dscvit.keats.ui.profile
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,10 +17,13 @@ import com.dscvit.keats.databinding.FragmentUserProfileBinding
 import com.dscvit.keats.model.Result
 import com.dscvit.keats.model.profile.UserEntity
 import com.dscvit.keats.ui.activities.PostAuthActivity
+import com.dscvit.keats.ui.activities.PreAuthActivity
 import com.dscvit.keats.utils.Constants
+import com.dscvit.keats.utils.PreferenceHelper
 import com.dscvit.keats.utils.disable
 import com.dscvit.keats.utils.enable
 import com.dscvit.keats.utils.hide
+import com.dscvit.keats.utils.shortToast
 import com.dscvit.keats.utils.show
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -37,6 +43,19 @@ class UserProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPreferences: SharedPreferences =
+            PreferenceHelper.customPrefs(requireContext(), Constants.PREF_NAME)
+        binding.logoutButton.setOnClickListener {
+            sharedPreferences.edit {
+                remove(Constants.PREF_AUTH_KEY)
+                remove(Constants.PREF_IS_LOGGED_IN)
+                commit()
+            }
+            val intent = Intent(requireActivity(), PreAuthActivity::class.java)
+            requireActivity().startActivity(intent)
+            requireActivity().finish()
+            context?.shortToast("Successfully Logged Out")
+        }
         viewModel.getUserProfile().observe(
             viewLifecycleOwner,
             {
@@ -62,6 +81,8 @@ class UserProfileFragment : Fragment() {
     private fun showUserProfileViews(user: UserEntity) {
         binding.progressBar.hide()
         binding.progressBar.disable()
+        binding.logoutButton.show()
+        binding.logoutButton.enable()
         binding.coverPhoto.show()
         binding.coverPhoto.enable()
         binding.profilePhoto.show()
