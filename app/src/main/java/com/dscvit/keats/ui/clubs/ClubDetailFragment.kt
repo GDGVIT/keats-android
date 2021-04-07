768c7d83d20b7d53100f090a90e1dc6a6354df14
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2021-04-07 11:06:56 +0530

    feat: add qr code sharing

--- app/src/main/java/com/dscvit/keats/ui/clubs/ClubDetailFragment.kt
+++ app/src/main/java/com/dscvit/keats/ui/clubs/ClubDetailFragment.kt
@@ -1,7 +1,10 @@
 package com.dscvit.keats.ui.clubs
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +12,7 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.OvershootInterpolator
+import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
@@ -17,10 +21,12 @@ import androidx.navigation.fragment.navArgs
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.dscvit.keats.BuildConfig
 import com.dscvit.keats.R
 import com.dscvit.keats.databinding.FragmentClubDetailBinding
 import com.dscvit.keats.databinding.QrCodeDialogBinding
 import com.dscvit.keats.model.Result
+import com.dscvit.keats.model.clubs.GetClubDetailsData
 import com.dscvit.keats.utils.disable
 import com.dscvit.keats.utils.enable
 import com.dscvit.keats.utils.hide
@@ -28,6 +34,10 @@ import com.dscvit.keats.utils.longToast
 import com.dscvit.keats.utils.show
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 @AndroidEntryPoint
 class ClubDetailFragment : Fragment() {
@@ -39,6 +49,7 @@ class ClubDetailFragment : Fragment() {
     private lateinit var fabCloseAnimation: Animation
     private lateinit var openAnimation: Animation
     private var isFabMenuOpen = false
+    private lateinit var qrCode: Bitmap
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -54,6 +65,10 @@ class ClubDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getClubDetails(args.clubId)
+        val a = viewModel.encodeAsBitmap(args.clubId)
+        if (a != null) {
+            qrCode = a
+        }
         binding.uploadBookButton.setOnClickListener {
             if (isFabMenuOpen)
                 collapseFabMenu()
@@ -63,6 +78,37 @@ class ClubDetailFragment : Fragment() {
         binding.showQr.setOnClickListener {
             showQR()
         }
+        binding.shareQr.setOnClickListener {
+            shareQR()
+        }
+    }
+
+    private fun shareQR() {
+        val filesDir: File? = context?.applicationContext?.filesDir
+        val imageFile = File(filesDir, "${args.clubId}.png")
+        val os: OutputStream
+        try {
+            os = FileOutputStream(imageFile)
+            qrCode.compress(Bitmap.CompressFormat.PNG, 100, os)
+            os.flush()
+            os.close()
+        } catch (e: Exception) {
+            Timber.e(e, javaClass.simpleName, "Error writing bitmap")
+        }
+        val intent = Intent()
+        intent.action = Intent.ACTION_SEND
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        val imageUri: Uri? =
+            context?.let {
+                FileProvider.getUriForFile(
+                    it,
+                    BuildConfig.APPLICATION_ID,
+                    imageFile
+                )
+            }
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri)
+        intent.type = "image/*"
+        context?.startActivity(intent)
     }
 
     private fun getClubDetails(clubId: String) {
@@ -78,34 +124,7 @@ class ClubDetailFragment : Fragment() {
                         binding.clubDetailsProgressBar.hide()
                         binding.clubDetailsProgressBar.disable()
                         if (it.data?.Status == "success") {
-                            binding.clubNameHeading.text = it.data.Data.Club.ClubName
-                            val clubImg = binding.clubPhotoDetailsPage
-                            val imgUrl =
-                                it.data.Data.Club.ClubPic.toUri().buildUpon().scheme("https")
-                                    .build()
-                            val circularProgressDrawable =
-                                CircularProgressDrawable(requireContext())
-                            circularProgressDrawable.strokeWidth = 5f
-                            circularProgressDrawable.centerRadius = 30f
-                            circularProgressDrawable.setColorSchemeColors(
-                                Color.argb(100, 244, 121, 18)
-                            )
-                            circularProgressDrawable.start()
-                            Glide.with(clubImg.context)
-                                .load(imgUrl)
-                                .apply(
-                                    RequestOptions()
-                                        .placeholder(circularProgressDrawable)
-                                        .error(R.drawable.ic_broken_image)
-                                )
-                                .into(clubImg)
-                            binding.clubNameHeading.show()
-                            binding.clubNameHeading.enable()
-                            binding.clubDetailsCard.startAnimation(openAnimation)
-                            binding.clubDetailsCard.show()
-                            binding.clubDetailsCard.enable()
-                            binding.showQr.show()
-                            binding.showQr.enable()
+                            showClubDetails(it.data.Data)
                         }
                     }
                     Result.Status.ERROR -> {
@@ -117,9 +136,8 @@ class ClubDetailFragment : Fragment() {
     }
 
     private fun showQR() {
-        val qrCodeImg = viewModel.encodeAsBitmap(args.clubId)
         val qrDialog = QrCodeDialogBinding.inflate(layoutInflater)
-        qrDialog.qrCode.setImageBitmap(qrCodeImg)
+        qrDialog.qrCode.setImageBitmap(qrCode)
         MaterialAlertDialogBuilder(requireContext())
             .setView(qrDialog.root)
             .setBackground(ColorDrawable(Color.TRANSPARENT))
@@ -144,5 +162,38 @@ class ClubDetailFragment : Fragment() {
         binding.floatingActionUploadPdf.isClickable = false
         binding.floatingActionUploadEpub.isClickable = false
         isFabMenuOpen = false
+    }
+
+    private fun showClubDetails(data: GetClubDetailsData) {
+        binding.clubNameHeading.text = data.Club.ClubName
+        val clubImg = binding.clubPhotoDetailsPage
+        val imgUrl =
+            data.Club.ClubPic.toUri().buildUpon().scheme("https")
+                .build()
+        val circularProgressDrawable =
+            CircularProgressDrawable(requireContext())
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.setColorSchemeColors(
+            Color.argb(100, 244, 121, 18)
+        )
+        circularProgressDrawable.start()
+        Glide.with(clubImg.context)
+            .load(imgUrl)
+            .apply(
+                RequestOptions()
+                    .placeholder(circularProgressDrawable)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(clubImg)
+        binding.clubNameHeading.show()
+        binding.clubNameHeading.enable()
+        binding.clubDetailsCard.startAnimation(openAnimation)
+        binding.clubDetailsCard.show()
+        binding.clubDetailsCard.enable()
+        binding.showQr.show()
+        binding.showQr.enable()
+        binding.shareQr.show()
+        binding.shareQr.enable()
     }
 }
